2102d8617fa1b9af4965cb3cc4cc233387ec57cf
Ольга Черепанова <chernichka.ekb@gmail.com>
2021-11-25 20:13:11 +0500

    Добавьте файлы проекта.

---
+++ file/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirName = "C:\\Автоматизация BIM проектирования\\";
+            string root = ".";
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName, root, SearchOption.AllDirectories);
+                foreach (string s in dirs)
+                {
+
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName, root, SearchOption.AllDirectories);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ text/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = "Log/file.txt";
+
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+
+            }
+
+
+            int[] numbers = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = random.Next(-100, 100);
+
+            }
+            using (StreamWriter sw = new StreamWriter(path, false))
+
+                foreach (int i in numbers)
+                {
+                    sw.Write("{0,-5}", random.Next(-100, 100));
+
+                }
+
+            Process.Start("C:\\Автоматизация bim проектирования\\С#\\Домашка 8\\Lab8\\text\\bin\\Debug\\Log\\file.txt");
+
+
+            int Sum = 0;
+
+            foreach (string line in File.ReadLines("C:\\Автоматизация bim проектирования\\С#\\Домашка 8\\Lab8\\text\\bin\\Debug\\Log\\file.txt"))
+            {
+                Console.WriteLine(line);
+                string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string str in parts)
+                {
+                    Sum += int.Parse(str);
+
+                }
+                Console.WriteLine(Sum);
+
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
